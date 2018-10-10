5d46470d15e50cbea274c910ca472067dc890ec2
Kai Matolat <kaimatolat@hotmail.com>
2018-10-10 18:43:11 +1000

    Added login system, need to eliminate the risk of a SQL Injection attack and cross scripting.

--- assets/pages/application.php
+++ assets/pages/application.php
@@ -2,7 +2,7 @@
 // Initialize the session
 session_start();
  
-// Check if the user is logged in, if not then redirect him to login page
+// Check if the user is logged in, if not then redirect them to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
@@ -10,27 +10,29 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 ?>
 
 <!DOCTYPE html>
-<html lang="en-AU">
-<head>
-<title>Welcome!</title>
+<html lang=en_AU>
+<meta charset="UTF-8">
 <style>
 .heading {
     text-align: center;
 }
 </style>
-<meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="assets/css/main.css">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-</head>
-<body>
-<div class="w3-container w3-padding-64 w3-center" id="team">
-<div class="shadow-lg p-3 mb-5 bg-white rounded">
-<div class="heading">
-<h1>Hello, <?php echo $_SESSION['username'] ?>! Here is your Welcome page!</h1>
-<br>
-<a href="application.php" button type="button" class="btn btn-primary">Application</button>
-<a href="logout.php" button type="button" class="btn btn-danger">Logout</button>
+<body id="mainApplication">
+<div class="w3-container w3-padding-64 w3-center" id="main">
+    <div class="shadow-sm p-3 mb-5 bg-white rounded">
+        <div class="heading">
+            <div class="p-3 mb-2 bg-warning text-white">
+                <h1>Main Application</h1>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+<script src="../js/all.js"></script>
+</html>
